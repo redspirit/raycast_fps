857b5298b865f44dd5eaf1ed4cb801fa771d96e3
Alexey <redspirit2@ya.ru>
2020-03-31 11:07:49 +0300

    server

---
+++ server.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public', {
+    index: 'index.html'
+}));
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({
+        success: false,
+        message: 'bad request'
+    });
+});
+
+app.listen(9090, () => {
+    console.log('PRICE Service listening on port', 9090);
+});
