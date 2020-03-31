608b4c93cd6687c1e97ac2d72d46435e8b1e5fd1
Alexey <redspirit2@ya.ru>
2020-03-31 13:18:15 +0300

    draw preview map

--- public/js/main.js
+++ public/js/main.js
@@ -21,15 +21,34 @@ let update = (ctx, delta) => {
 
     drawBlocks(ctx, x + 400, y + 300);
 
-    ctx.font = "14px Arial";
-    ctx.fillText("Delta: " + delta, 10, 50);
+    // ctx.font = "14px Arial";
+    // ctx.fillText("Delta: " + delta, 10, 50);
 
 };
 
+let drawPreview = (ctx) => {
+
+
+
+    ctx.strokeStyle = "rgb(0,0,0)";
+    MapData.forEach(poly => {
+
+        ctx.beginPath();
+        ctx.moveTo(poly[0].x, poly[0].y);
+        for (let i = 1; i < poly.length; i++ ) {
+            ctx.lineTo(poly[i].x, poly[i].y);
+        }
+        ctx.lineTo(poly[0].x, poly[0].y);
+        ctx.stroke();
+    });
+
+};
+
+
 engine.addUpdateCallback(update);
+engine.addUpdateCallback(drawPreview);
 
 engine.start();
 
-let res = Intersection.intersectLinePolygon({x:2,y:6}, {x:7,y:3}, [{x:2,y:3}, {x:2,y:2}, {x:6,y:2}, {x:6,y:3}]);
-
-console.log(res);
+// let res = Intersection.intersectLinePolygon({x:2,y:6}, {x:7,y:3}, [{x:2,y:3}, {x:2,y:2}, {x:6,y:2}, {x:6,y:3}]);
+// console.log(res);
